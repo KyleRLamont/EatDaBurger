96cc8d0078da6f71eb82e3f2705b3e94488f390a
Kyle LaMont <kylerlamont@gmail.com>
2019-12-02 13:25:14 -0800

    created database for burger info and populated database

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Whopper', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Big MAc', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Quarter Pounder', false);
