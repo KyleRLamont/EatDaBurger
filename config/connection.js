30be46424a59215702ee08ee7dda52f7dab5bef9
Kyle LaMont <kylerlamont@gmail.com>
2019-12-04 21:03:55 -0800

    added jawsdb to address db connection issue

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 
-var connection
+var connection;
 
-if (process.env.CLEARDB_TEAL_URL) {
-    connection = mysql.createConnection(process.env.CLEARDB_TEAL_URL)
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
     connection = mysql.createConnection({
         host: "localhost",
