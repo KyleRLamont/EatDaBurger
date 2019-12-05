beb657e193b82db759b7818c3067178f4e6e8efe
Kyle LaMont <kylerlamont@gmail.com>
2019-12-04 20:56:34 -0800

    added cleardb url code in connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burger_db"
-});
+var connection
+
+if (process.env.CLEARDB_TEAL_URL) {
+    connection = mysql.createConnection(process.env.CLEARDB_TEAL_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burger_db"
+    })
+};
 
 connection.connect(function (err) {
     if (err) {
